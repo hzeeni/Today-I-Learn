ad1b12bb58ce27086e3b3ebb4a23872d5b5b3e69
hzeen <hzeen0301@gmail.com>
2022-09-01 18:26:39 +0900

    HttpServletResponse_응답 데이터

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,36 @@
+package hello.servlet.basic.response;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hello.servlet.basic.HelloData;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet{
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Content-Type: application/json
+		 response.setHeader("content-type", "application/json");
+		 response.setCharacterEncoding("utf-8");
+		 
+		 HelloData data = new HelloData();
+		 data.setUsername("lim");
+		 data.setAge(28);
+		 
+		 //{"username":"lim","age":28}
+		 String result = objectMapper.writeValueAsString(data);
+		 response.getWriter().write(result);
+	
+	}
+
+}
