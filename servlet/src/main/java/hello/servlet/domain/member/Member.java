8d0f1574db27c9a71b6d5e4cf9fbb226ded03b71
hzeen <hzeen0301@gmail.com>
2022-09-03 19:47:58 +0900

    서블릿과 JSP

---
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,27 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+	
+	private Long id;
+	private String username;
+	private int age;
+	
+	public Member() {
+		
+	}
+	
+	/**
+	 * @param username
+	 * @param age
+	 */
+	public Member(String username, int age) {
+		this.username = username;
+		this.age = age;
+	}
+
+	
+}
